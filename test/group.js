02c9e37b90ed75c85b523080e91166400665b8e0
Vasyl Moskalov <vasyl@h2i.sg>
2022-09-10 10:50:54 +0800

    Update attribute testing

--- test/group.js
+++ test/group.js
@@ -7,25 +7,43 @@ const netcdf4 = require("..");
 const { copyFileSync, unlinkSync } = require("fs");
 const { tmpdir } = require("os");
 const { join } = require("path");
+const uniqueFilename = require('unique-filename');
 
 const fixture = join(__dirname, "test_hgroups.nc");
+const fixture1 = join(__dirname, "testrh.nc");
 
 describe("Group", function () {
-  let file,tempFileName;
+  let file;
+  let files=[];
+
+  const newFile=async (name=fixture,mode='w')=>{
+    const tempFileName = uniqueFilename(tmpdir(), `orc`);
+    copyFileSync(fixture, tempFileName);
+    const file=await netcdf4.open(tempFileName, mode);
+    files.push(file);
+    return file;
+  };
+
+  const closeAll=async ()=>{
+    for (const f of files) {
+      try{
+        if (!f.closed) {
+          await f.close();
+        }
+        unlinkSync(f.name);          
+      } catch (e) {
+        console.log(`Exception ${e.message} due file ${file.name} closing`);
+      }
+    };
+    files=[];
+  };
 
   beforeEach(async function () {
-    tempFileName = join(tmpdir(), `${Date.now()}.rc`)
-    copyFileSync(fixture, tempFileName);
-    file = await netcdf4.open(tempFileName, "w");
+    file = await newFile();
   });
 
   afterEach(async function () {
-  try{
-    await file.close();
-    unlinkSync(tempFileName);
-  } catch (ignore) {
-    console.log(`Got ${ignore.message} during file ${file?file:"undefined"} closing, ingored`)
-  }
+    await closeAll();
   });
 
   it("should read name", async  function () {
@@ -93,7 +111,7 @@ describe("Group", function () {
     await expect(subgroup.getName()).become('new_mozaic_flight');
     await expect(subgroup.getPath()).become('/new_mozaic_flight');
     await file.close();
-    file = await netcdf4.open(tempFileName, "r");
+    file = await netcdf4.open(file.name, "r");
     var subgroups = await expect(file.root.getSubgroups()).to.be.fulfilled;
     var subgroup = subgroups.new_mozaic_flight;
     await expect(subgroup.getName()).become('new_mozaic_flight');
@@ -111,7 +129,7 @@ describe("Group", function () {
     await expect(newSubGroup.getPath()).become('/new_group/new_nested_group');
     await expect(newGroup.getSubgroups()).eventually.to.have.property("new_nested_group");
     await file.close();
-    file = await new netcdf4.open(tempFileName, "r");
+    file = await new netcdf4.open(file.name, "r");
     var subgroup = (await file.root.getSubgroups()).new_group;
     await expect(subgroup.getName()).become('new_group');
     await expect(subgroup.getPath()).become('/new_group');
@@ -128,7 +146,7 @@ describe("Group", function () {
     await expect(file.root.addDimension("new_dim",10)).eventually.to.be.deep.eq({"new_dim":10});
     await expect(file.root.getDimensions()).eventually.to.have.property("new_dim").to.be.deep.eq(10);
     await file.close();
-    file = await netcdf4.open(tempFileName, "r");
+    file = await netcdf4.open(file.name, "r");
     await expect(file.root.getDimensions()).eventually.to.have.property("new_dim").to.be.deep.eq(10);
   });
 
@@ -139,7 +157,7 @@ describe("Group", function () {
     await expect(file.root.getDimensions()).eventually.to.have.property("new_dim").to.be.deep.eq("unlimited");
     await expect(file.root.getDimensions(true)).eventually.to.have.property("new_dim").to.be.deep.eq("unlimited");
     await file.close();
-    file = await netcdf4.open(tempFileName, "r");
+    file = await netcdf4.open(file.name, "r");
     await expect(file.root.getDimensions()).eventually.to.have.property("new_dim").to.be.deep.eq("unlimited");
     await expect(file.root.getDimensions(true)).eventually.to.have.property("new_dim").to.be.deep.eq("unlimited");
   });
@@ -156,7 +174,7 @@ describe("Group", function () {
     var vars=await file.root.getVariables();
     expect(vars.test_variable.inspect(),'[Variable test_variable, type byte, 1 dimension(s)]');
     await file.close();
-    file = await netcdf4.open(tempFileName, "r");
+    file = await netcdf4.open(file.name, "r");
     await expect(file.root.getVariables()).eventually.to.have.property("test_variable");
     vars=await file.root.getVariables();
     expect(vars.test_variable.inspect(),'[Variable test_variable, type byte, 1 dimension(s)]');
@@ -179,17 +197,62 @@ describe("Group", function () {
     expect(attributes).to.have.property("time_arr");
   });
 
-  it("should add attribute",async function() {
-    await expect(file.root.getAttributes()).eventually.to.not.have.property("root_attr_prop");
-    const attr=await expect(file.root.addAttribute("root_attr_prop","string","root attr property")).to.be.fulfilled;
-    expect(attr).deep.equal({"root_attr_prop":{"type":"string","value":"root attr property"}});
-    expect(file.root.getAttributes()).eventually.to.have.property("root_attr_prop");
-    await file.close();
-    file = await netcdf4.open(tempFileName, "r");
-    await expect(file.root.getAttributes()).eventually.to.have.property("root_attr_prop");
-    await expect(file.root.getAttributes()).eventually.to.have.property("root_attr_prop").to.deep.property("value").to.deep.equal("root attr property");
-    await expect(file.root.getAttributes(true)).eventually.to.have.property("root_attr_prop").to.deep.equal({"type":"string","value":"root attr property"});
-  })  
+  const arrTypes={
+    "byte":[Int8Array,Number],
+    "short":[Int16Array,Number],
+    "int":[Int32Array,Number],
+    "float":[Float32Array,Number],
+    "double":[Float64Array,Number],
+    "ubyte":[Uint8Array,Number],
+    "ushort":[Uint16Array,Number],
+    "uint":[Uint32Array,Number],
+    "string":[Array.from,String]
+  };
+  if (process.versions.node.split(".")[0]>=10) {
+    arrTypes["uint64"]=[BigUint64Array,BigInt];
+    arrTypes["int64"]=[BigInt64Array,BigInt];
+  };
+  
+  const testAddAttr=(fileType,type,value)=>{
+
+    it(`${fileType} should add attribute type ${type}`,async function() {
+      let file=await newFile(fileType==='hdf5'?fixture1:fixture);
+      await expect(file.root.getAttributes()).eventually.to.not.have.property("root_attr_prop");
+      const attr=await expect(file.root.addAttribute("root_attr_prop",type,value)).to.be.fulfilled;
+      expect(attr).deep.equal({"root_attr_prop":{"type":type,"value":value}});
+      expect(file.root.getAttributes()).eventually.to.have.property("root_attr_prop");
+      await file.close();
+      file = await netcdf4.open(file.name, "r");
+      await expect(file.root.getAttributes()).eventually.to.have.property("root_attr_prop");
+      await expect(file.root.getAttributes()).eventually.to.have.property("root_attr_prop").to.deep.property("value").to.deep.equal(value);
+      await expect(file.root.getAttributes(true)).eventually.to.have.property("root_attr_prop").to.deep.equal({"type":type,"value":value});
+    })  
+  }
+  
+
+
+  const testSuiteOld=[
+    ['byte',10,[10,20,30,40],127],
+    ['short',1024,[20,512,333,1024],32767],
+    ['int',100000,[0,-200,3000,555666],32767],
+    ['float',153.2,[-12,33,55.5,106.2],-555.555],
+    ['double',153.2,[-12,33,55.5,106.2],-555.555],
+    ['ubyte',10,[10,20,30,40],127],
+    ['ushort',1024,[20,512,333,1024],127],
+    ['uint',100000,[0,200,3000,555666],127],
+    ['string',"Test value",["111","222","333","444"],"fill"]
+  ];
+  if (process.versions.node.split(".")[0]>=10) {
+    testSuiteOld.push(['uint64',1024,[20,512555,333,77788889].map(v=>BigInt(v)),BigInt(100)]);
+    testSuiteOld.push(['int64',100000,[0,200,3000,555666].map(v=>BigInt(v)),BigInt(100)]);
+  };
+  testSuiteOld
+  .forEach(v=>testAddAttr('hdf5',v[0],arrTypes[v[0]][1](v[1])));
+  testSuiteOld
+  .filter(v=>['ubyte','ushort','uint','string','int64','uint64'].indexOf(v[0])===-1)
+  .forEach(v=>testAddAttr('netcdf3',v[0],arrTypes[v[0]][1](v[1])));
+
+
 
   // it("should add attribute",async function() {
   //   await expect(file.root.getAttributes()).eventually.to.not.have.property("root_attr_prop");
