2d52b4e6e4e8a07b39ddf71180ef2852de4277c0
Vasyl Moskalov <vasyl@h2i.sg>
2022-09-19 12:45:13 +0800

    Attempt to solve error LNK2001 unresolved external symbol under windows

--- src/Attribute.h
+++ src/Attribute.h
@@ -7,11 +7,18 @@
 
 namespace netcdf4async {
 
+#ifdef _MSC_VER
+extern "C" {
+#endif	
+
 Napi::Value add_attribute(Napi::Env env, Napi::Promise::Deferred deferred, int parent_id, int var_id,
     const std::string attribute_name, int type, const Napi::Value &value);
 Napi::Promise::Deferred get_attributes(Napi::Env env, int parent_id, int var_id, bool return_type);
 Napi::Promise::Deferred rename_attribute(Napi::Env env, Napi::Promise::Deferred deferred, int parent_id, int var_id, std::string old_name, std::string new_name);
 Napi::Promise::Deferred delete_attribute(Napi::Env env, Napi::Promise::Deferred deferred, int parent_id, int var_id, std::string name);
+#ifdef _MSC_VER
+}
+#endif	
 
 
 }
